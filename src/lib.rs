0e7eacaead17b2138d4e1e457e8888ea61948ffe
Henrik Vik <henkvik@gmail.com>
2018-11-20 23:32:34 +0100

    square yes with update

--- src/lib.rs
+++ src/lib.rs
@@ -1,255 +1,382 @@
+extern crate console_error_panic_hook;
+extern crate nalgebra as na;
 extern crate wasm_bindgen;
 extern crate web_sys;
-extern crate nalgebra as na;
 
 use js_sys::WebAssembly;
+use std::f32;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlCanvasElement, WebGlProgram, WebGl2RenderingContext, WebGlShader, WebGlBuffer};
-use web_sys::WebGl2RenderingContext as gl;
-use std::f32;
+use web_sys::WebGl2RenderingContext as GL;
+use web_sys::{Document, Window};
+use web_sys::{HtmlCanvasElement, WebGlBuffer, WebGlProgram, WebGlShader, WebGlUniformLocation};
 
+static VERTEX_SOURCE: &str = "
+attribute vec4 vertex_position;
+attribute vec4 vertex_color;
 
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+varying lowp vec4 color;
+
+void main() {
+    gl_Position = projection * view * model * vertex_position;
+    color = vertex_color;
+}";
+
+static FRAGMENT_SOURCE: &str = "
+varying lowp vec4 color;
+
+void main() {
+    gl_FragColor = color;
+}";
 
 #[wasm_bindgen]
-pub fn main() -> Result<(), JsValue> {
-    let width = 400;
-    let height = 400;
+extern "C" {
+    #[wasm_bindgen(js_namespace=console)]
+    fn log(s: &str);
+}
 
-    let window = web_sys::window().expect("no window");
-    let document = window.document().expect("no document");
-    add_style(&document, "
-        html { 
-            background: #DDD;
-            margin: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-    ");
+#[wasm_bindgen]
+pub struct App {
+    window: Window,
+    document: Document,
+    canvas: HtmlCanvasElement,
 
-    let canvas: HtmlCanvasElement = document.create_element("canvas")?.dyn_into()?;
-    canvas.set_width(width);
-    canvas.set_height(height);
-    document.body().expect("no body").append_child(&canvas)?;
+    renderer: Renderer,
 
-    let context: WebGl2RenderingContext = canvas.get_context("webgl2")?.unwrap().dyn_into()?;
+    i: f32,
+}
 
-    context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.clear_depth(1.0);
-    context.enable(gl::DEPTH_TEST);
-    context.depth_func(gl::LEQUAL);
-    context.clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+#[wasm_bindgen]
+impl App {
+    pub fn new() -> Result<App, JsValue> {
+        console_error_panic_hook::set_once();
 
-    let vs_source = "
-        attribute vec4 vertex_position;
-        attribute vec4 vertex_color;
+        let width = 400;
+        let height = 400;
 
-        uniform mat4 model_view_matrix;
-        uniform mat4 projection_matrix;
-
-        varying lowp vec4 color;
-
-        void main() {
-            gl_Position = projection_matrix * model_view_matrix * vertex_position;
-            color = vertex_color;
-        }
-    ";
-
-    let fs_source = "
-        varying lowp vec4 color;
-
-        void main() {
-            gl_FragColor = color;
-        }
-    ";
-
-    let vertex_shader   = load_shader(&context, gl::VERTEX_SHADER, vs_source)?;
-    let fragment_shader = load_shader(&context, gl::FRAGMENT_SHADER, fs_source)?;
-    let program         = link_shaders(&context, &vertex_shader, &fragment_shader)?;
-
-
-    let vertex_position_location   = context.get_attrib_location(&program, "vertex_position") as u32;
-    let vertex_color_location      = context.get_attrib_location(&program, "vertex_color") as u32;
-    let model_view_matrix_location = context.get_uniform_location(&program, "model_view_matrix").expect("uniform not found");
-    let projection_matrix_location = context.get_uniform_location(&program, "projection_matrix").expect("uniform not found");
-
-    let buffers = init_buffers(&context);
-
-    let projection = na::Perspective3::new(
-        canvas.width() as f32 / canvas.height() as f32, 
-        45.0 * f32::consts::PI / 180.0, 
-        0.1, 
-        100.0
-    );
-
-    let model = na::Isometry3::<f32>::new(na::zero(), na::zero());
-    let view = na::Isometry3::<f32>::look_at_rh(
-        &na::Point3::new(0.0, 0.0, 5.0), 
-        &na::Point3::new(0.0, 0.0, 0.0), 
-        &na::Vector3::y()
-    );
-    
-    let model_view = view * model;
-
-    {
-        let num_components = 2;
-        let data_type = gl::FLOAT;
-        let normalize = false;
-        let stride = 0;
-        let offset = 0;
-        context.bind_buffer(gl::ARRAY_BUFFER, Some(&buffers.position));
-        context.enable_vertex_attrib_array(vertex_position_location);
-        context.vertex_attrib_pointer_with_i32(
-            vertex_position_location,
-            num_components,
-            data_type,
-            normalize,
-            stride,
-            offset
+        let window = web_sys::window().expect("no window");
+        let document = window.document().expect("no document");
+        Self::add_style(
+            &document,
+            "html { 
+                background: #DDD;
+                margin: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }",
         );
 
-        context.bind_buffer(gl::ARRAY_BUFFER, Some(&buffers.color));
-        context.enable_vertex_attrib_array(vertex_color_location);
-        context.vertex_attrib_pointer_with_i32(
-            vertex_color_location,
-            4,
-            gl::FLOAT,
+        let canvas: HtmlCanvasElement = document.create_element("canvas")?.dyn_into()?;
+
+        canvas.set_width(width);
+        canvas.set_height(height);
+        document.body().expect("no body").append_child(&canvas)?;
+
+        let gl: GL = canvas.get_context("webgl2")?.unwrap().dyn_into()?;
+
+        Ok(App {
+            window,
+            document,
+            canvas,
+            renderer: Renderer::new(gl)?,
+            i: 0.0,
+        })
+    }
+
+    pub fn update(&mut self) {
+        // log(&format!("{}",self.i));
+
+        self.i += 0.01;
+
+        self.renderer.clear();
+        self.renderer.draw();
+    }
+
+    fn add_style(document: &Document, css: &str) {
+        let style = document.create_element("style").unwrap();
+        style.set_inner_html(css);
+        document.head().unwrap().append_child(&style).unwrap();
+    }
+}
+
+struct Renderer {
+    gl: GL,
+    program: ShaderProgram,
+    buffers: Buffers,
+    camera: Camera,
+}
+
+impl Renderer {
+    fn new(gl: GL) -> Result<Renderer, JsValue> {
+        gl.clear_color(0.0, 0.0, 0.0, 1.0);
+        gl.clear_depth(1.0);
+        gl.enable(GL::DEPTH_TEST);
+        gl.depth_func(GL::LEQUAL);
+        gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+
+        let program = ShaderProgram::new(&gl, VERTEX_SOURCE, FRAGMENT_SOURCE)?;
+        program.use_program(&gl);
+
+        let buffers = Buffers {
+            position: Buffer::new(
+                &gl,
+                program.attributes.position,
+                2,
+                Box::new([
+                    -1.0,  1.0, 
+                     1.0,  1.0, 
+                    -1.0, -1.0, 
+                     1.0, -1.0
+                ]),
+            ),
+            color: Buffer::new(
+                &gl,
+                program.attributes.color,
+                4,
+                Box::new([
+                    1.0, 0.0, 0.0, 1.0, 
+                    0.0, 1.0, 0.0, 1.0, 
+                    0.0, 0.0, 1.0, 1.0, 
+                    1.0, 1.0, 1.0, 1.0,
+                ]),
+            ),
+        };
+
+
+        let camera = Camera::new(
+            400.0 / 400.0, 
+            45.0,
+            na::Point3::new(0.0, 0.0, 5.0),
+            na::Point3::new(0.0, 0.0, 0.0),
+        );
+
+
+        gl.uniform_matrix4fv_with_f32_array(
+            Some(&program.uniforms.projection),
             false,
-            0,
-            0
+            // na::Matrix4::<f32>::identity().as_mut_slice(),
+            camera.get_projection().as_mut_slice(),
         );
 
-    }
-
-    context.use_program(Some(&program));
-
-    context.uniform_matrix4fv_with_f32_array(
-        Some(&model_view_matrix_location),
-        false,
-        model_view.to_homogeneous().as_mut_slice()
-        // na::Matrix4::<f32>::identity().as_mut_slice()
-    );
-
-    context.uniform_matrix4fv_with_f32_array(
-        Some(&projection_matrix_location),
-        false,
-        projection.to_homogeneous().as_mut_slice()
-        // na::Matrix4::<f32>::identity().as_mut_slice()
-    );
-
-
-    context.draw_arrays(gl::TRIANGLE_STRIP, 0, 4);
-
-
-
-    while true {
-        
-
-        context.draw_arrays(gl::TRIANGLE_STRIP, 0, 4);
-        context.uniform_matrix4fv_with_f32_array(
-            Some(&model_view_matrix_location),
+        gl.uniform_matrix4fv_with_f32_array(
+            Some(&program.uniforms.view),
             false,
-            (view * model).to_homogeneous().as_mut_slice()
-            // na::Matrix4::<f32>::identity().as_mut_slice()
+            // na::Matrix4::<f32>::identity().as_mut_slice(),
+            camera.get_view().as_mut_slice(),
         );
+
+        gl.uniform_matrix4fv_with_f32_array(
+            Some(&program.uniforms.model),
+            false,
+            na::Matrix4::<f32>::identity().as_mut_slice(),
+        );
+
+        Ok(Renderer {
+            gl,
+            program,
+            buffers,
+            camera,
+        })
     }
 
+    fn clear(&self) {
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+    }
 
-    Ok(())
-}
-
-fn add_style(document: &web_sys::Document, css: &str) {
-    let style = document.create_element("style").unwrap();
-    style.set_inner_html(css);
-    document.head().unwrap().append_child(&style).unwrap();
-}
-
-fn load_shader(context : &WebGl2RenderingContext, shader_type : u32, shader_source : &str) -> Result<WebGlShader, String> {
-    let shader = context.create_shader(shader_type).expect("failed to create shader");
-
-    context.shader_source(&shader, shader_source);
-    context.compile_shader(&shader);
-
-    if context.get_shader_parameter(&shader, gl::COMPILE_STATUS).as_bool().unwrap_or(false) {
-        Ok(shader)
-    } else {
-        let err = context.get_shader_info_log(&shader).unwrap_or("unknow shader compile error".into());
-        context.delete_shader(Some(&shader));
-        Err(err.into())
+    fn draw(&self) {
+        self.gl.draw_arrays(GL::TRIANGLE_STRIP, 0, 4);
     }
 }
 
-fn link_shaders(context : &gl, vertex_shader : &WebGlShader, fragment_shader : &WebGlShader) -> Result<WebGlProgram, String> {
-    let program : WebGlProgram = context.create_program().unwrap();
-    context.attach_shader(&program, &vertex_shader);
-    context.attach_shader(&program, &fragment_shader);
-    context.link_program(&program);
+struct ShaderProgram {
+    program: WebGlProgram,
+    attributes: AttributeLocations,
+    uniforms: UniformLocations,
+}
 
-    if context.get_program_parameter(&program, gl::LINK_STATUS).as_bool().unwrap_or(false) {
-        Ok(program)
-    } else {
-        let err = context.get_program_info_log(&program).unwrap_or("unknow program error".into());
-        context.delete_program(Some(&program));
-        Err(err.into())
+impl ShaderProgram {
+    fn new(gl: &GL, vertex_source: &str, fragment_source: &str) -> Result<ShaderProgram, JsValue> {
+        let vertex_shader = Self::load_shader(&gl, GL::VERTEX_SHADER, vertex_source)?;
+        let fragment_shader = Self::load_shader(&gl, GL::FRAGMENT_SHADER, fragment_source)?;
+
+        let program = Self::link_program(&gl, &vertex_shader, &fragment_shader)?;
+
+        let position = gl.get_attrib_location(&program, "vertex_position") as u32;
+
+        let color = gl.get_attrib_location(&program, "vertex_color") as u32;
+
+        let attributes = AttributeLocations { position, color };
+
+        let model = gl
+            .get_uniform_location(&program, "model")
+            .expect("uniform not found");
+        let view = gl
+            .get_uniform_location(&program, "view")
+            .expect("uniform not found");
+        let projection = gl
+            .get_uniform_location(&program, "projection")
+            .expect("uniform not found");
+
+        let uniforms = UniformLocations {
+            model,
+            view,
+            projection,
+        };
+
+        Ok(ShaderProgram {
+            program,
+            attributes,
+            uniforms,
+        })
     }
+
+    fn use_program(&self, gl: &GL) {
+        gl.use_program(Some(&self.program));
+        gl.enable_vertex_attrib_array(self.attributes.position);
+        gl.enable_vertex_attrib_array(self.attributes.color);
+    }
+
+    fn load_shader(gl: &GL, shader_type: u32, shader_source: &str) -> Result<WebGlShader, JsValue> {
+        let shader = gl
+            .create_shader(shader_type)
+            .expect("failed to create shader");
+
+        gl.shader_source(&shader, shader_source);
+        gl.compile_shader(&shader);
+
+        if gl
+            .get_shader_parameter(&shader, GL::COMPILE_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(shader)
+        } else {
+            let err = gl
+                .get_shader_info_log(&shader)
+                .unwrap_or("unknow shader compile error".into());
+            gl.delete_shader(Some(&shader));
+            Err(err.into())
+        }
+    }
+
+    fn link_program(
+        gl: &GL,
+        vertex_shader: &WebGlShader,
+        fragment_shader: &WebGlShader,
+    ) -> Result<WebGlProgram, String> {
+        let program = gl.create_program().unwrap();
+
+        gl.attach_shader(&program, &vertex_shader);
+        gl.attach_shader(&program, &fragment_shader);
+        gl.link_program(&program);
+
+        if gl
+            .get_program_parameter(&program, GL::LINK_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(program)
+        } else {
+            let err = gl
+                .get_program_info_log(&program)
+                .unwrap_or("unknow program error".into());
+            gl.delete_program(Some(&program));
+            Err(err.into())
+        }
+    }
+}
+
+struct AttributeLocations {
+    position: u32,
+    color: u32,
+}
+
+struct UniformLocations {
+    model: WebGlUniformLocation,
+    view: WebGlUniformLocation,
+    projection: WebGlUniformLocation,
 }
 
 struct Buffers {
-    position : WebGlBuffer,
-    color : WebGlBuffer,
+    position: Buffer,
+    color: Buffer,
 }
 
-fn init_buffers(context : &gl) -> Buffers {
+struct Buffer {
+    data: Box<[f32]>,
+    buffer: WebGlBuffer,
+}
 
-    let positions : [f32;8] = [
-        -1.0,  1.0,
-         1.0,  1.0,
-        -1.0, -1.0,
-         1.0, -1.0
-    ];
+impl Buffer {
+    fn new(gl: &GL, location: u32, num_components: i32, data: Box<[f32]>) -> Buffer {
+        let buffer = gl.create_buffer().unwrap();
 
-    let position_buffer = context.create_buffer().unwrap();
-    context.bind_buffer(gl::ARRAY_BUFFER, Some(&position_buffer));
-    context.buffer_data_with_array_buffer_view(
-        gl::ARRAY_BUFFER, 
-        &f32_array_to_js_array_wrapper(&positions), 
-        gl::STATIC_DRAW
-    );
+        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
+        gl.vertex_attrib_pointer_with_i32(location, num_components, GL::FLOAT, false, 0, 0);
+        gl.buffer_data_with_array_buffer_view(
+            GL::ARRAY_BUFFER,
+            &Self::js_array_wrapper(data.as_ref()),
+            GL::STATIC_DRAW,
+        );
 
-    let colors : [f32;16] = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-    ];
+        Buffer { buffer, data }
+    }
 
-    let color_buffer = context.create_buffer().unwrap();
-    context.bind_buffer(gl::ARRAY_BUFFER, Some(&color_buffer));
-    context.buffer_data_with_array_buffer_view(
-        gl::ARRAY_BUFFER,
-        &f32_array_to_js_array_wrapper(&colors),
-        gl::STATIC_DRAW
-    );
+    fn js_array_wrapper(array: &[f32]) -> js_sys::Float32Array {
+        let array_location = array.as_ptr() as u32 / 4; // devided by 4 ????
 
+        let memory_buffer = wasm_bindgen::memory()
+            .dyn_into::<WebAssembly::Memory>()
+            .unwrap()
+            .buffer();
 
-    Buffers { 
-        position : position_buffer,
-        color : color_buffer,
+        js_sys::Float32Array::new(&memory_buffer).subarray(
+            //// WHO OWNS THIS?
+            array_location,
+            array_location + array.len() as u32,
+        )
     }
 }
 
-fn f32_array_to_js_array_wrapper(array : &[f32]) -> js_sys::Float32Array {
-    let array_location = array.as_ptr() as u32 / 4; // devided by 4 ????
+struct Camera {
+    view: na::Isometry3<f32>,
+    projection: na::Perspective3<f32>,
+}
 
-    let memory_buffer = wasm_bindgen::memory()          //
-        .dyn_into::<WebAssembly::Memory>().unwrap()     //
-        .buffer();                                      //
-                                                        //
-    js_sys::Float32Array::new(&memory_buffer).subarray( //// WHO OWNS THIS?
-        array_location,                                 //
-        array_location + array.len() as u32             //
-    )                                                   //
+impl Camera {
+    fn new(aspect : f32, fov : f32, eye: na::Point3<f32>, target: na::Point3<f32>) -> Camera {
+        let projection = na::Perspective3::new(aspect, Self::deg_to_rad(fov), 0.01, 100.0);
+        let view = na::Isometry3::look_at_rh(&eye, &target, &na::Vector3::y());
+
+        // let eye    = na::Point3::new(0.0, 0.0, -1.0);
+        // let target = na::Point3::new(0.0, 0.0, 0.0);
+        // let view   = na::Isometry3::look_at_rh(&eye, &target, &na::Vector3::y());
+
+        // let projection = na::Perspective3::new(16.0 / 9.0, 3.14 / 2.0, 1.0, 1000.0);
+
+
+        Camera {
+            view,
+            projection,
+        }
+    }
+
+    fn deg_to_rad(deg: f32) -> f32 {
+        deg * f32::consts::PI / 180.0
+    }
+
+    fn get_view(&self) -> na::Matrix4<f32> {
+        self.view.to_homogeneous()
+    }
+
+    fn get_projection(&self) -> na::Matrix4<f32> {
+        self.projection.to_homogeneous()
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,2 +1,19 @@
 let wasm = import("../target/webpack/wasm_pong");
-wasm.then(wasm => wasm.main());
+wasm.then(wasm => {
+
+    try {
+
+        let app = wasm.App.new();
+
+        let loop = ()=>{
+            app.update();
+            requestAnimationFrame(loop);
+        }
+
+        loop();
+
+    } catch (e) {
+        console.error(e);
+    }
+
+});
